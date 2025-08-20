0ebfb846aacafbf2ba89400a50add6fca473417a
sifat-samin <sifatsamin19@gmail.com>
2025-08-20 18:08:59 +0600

    Menu Class and Food Item Class Work done

--- Resturant_Management_System/user.py
+++ Resturant_Management_System/user.py
@@ -20,7 +20,7 @@ class Employee(User):
         self.salary = salary
 
     def __repr__(self):
-        return f'Name : {self.name} | Phone : {self.phone} | Email : {self.email} | Address : {self.address} | Age : {self.age} | Designation : {self.designation} | Salary : {self.salary}'
+        return f'Name : {self.name} |\t Phone : {self.phone} |\t Email : {self.email} |\t Address : {self.address} |\t Age : {self.age} |\t Designation : {self.designation} |\t Salary : {self.salary}'
 
 class Admin(User):
     def __init__(self, name, phone, email, address, age):
@@ -46,6 +46,43 @@ class Restaurant:
         for emp in self.employees:
             print(emp)
 
+class Menu:
+    def __init__(self):
+        self.items = [] # Database of menu items
+    
+    def add_menu_items(self, item):
+        self.items.append(item)
+    
+    def find_items(self, item_name):
+        for item in self.items:
+            if item.name.lower() == item_name.lower():
+                return item
+            return None    
+
+    def remove_item(self, item_name):
+        item = self.find_items(item_name)
+        
+        if item:
+            self.items.remove(item)
+            print("Items Deleted")
+
+        else:
+            print("Item not Found")
+
+    def show_menu_items(self):
+        print('**** MENU ****')
+        print('Name\tPrice\tQuantity')
+        for item in self.items:
+            print(f'{item.name}\t{item.price}\t{item.quantity}')
+
+class Food_Items:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+
+
 admin = Admin('Samin', '12352552', 'sifatsamin@gmail.com', 'Cumilla', 24)
 print(admin.name)
 print(admin.email)
@@ -58,5 +95,12 @@ admin.add_Employees(restaurant, Employee("Rahim", "01711111111", "rahim@gmail.co
 admin.add_Employees(restaurant, Employee("Karim", "01822222222", "karim@gmail.com", "Chittagong", 25, "Developer", 40000))
 
 admin.view_Employees(restaurant)
+
+menu = Menu()
+item = Food_Items('Pizza', 1200, 4)
+
+menu.add_menu_items(item)
+menu.show_menu_items()
+
 
         
