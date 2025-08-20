9c93d673be7a53e2a6c48fc3d3f2fab8a67bb53c
sifat-samin <sifatsamin19@gmail.com>
2025-08-20 21:08:01 +0600

    restaurant.py Added

---
+++ Resturant_Management_System/restaurant.py
@@ -0,0 +1,13 @@
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+        self.employees = [] # This is our employee database
+        self.menu = Menu()
+
+    def add_Employees(self, employee):
+        self.employees.append(employee)
+
+    def view_Employees(self):
+        print('Employee List : ')
+        for emp in self.employees:
+            print(emp)
--- Resturant_Management_System/user.py
+++ Resturant_Management_System/user.py
@@ -70,19 +70,7 @@ class Admin(User):
     def remove_item(self, restaurant, item):
         restaurant.menu.remove_item(item)
         
-class Restaurant:
-    def __init__(self, name):
-        self.name = name
-        self.employees = [] # This is our employee database
-        self.menu = Menu()
 
-    def add_Employees(self, employee):
-        self.employees.append(employee)
-
-    def view_Employees(self):
-        print('Employee List : ')
-        for emp in self.employees:
-            print(emp)
 
 class Menu:
     def __init__(self):
