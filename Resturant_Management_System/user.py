d9288146ae3dcd5158eb360de9494c12f585f0dd
sifat-samin <sifatsamin19@gmail.com>
2025-08-20 17:02:26 +0600

    User and Employee Class Created

--- Resturant_Management_System/user.py
+++ Resturant_Management_System/user.py
@@ -2,3 +2,26 @@
 # Admin
 # Employee
 # Customer
+
+from abc import ABC
+
+class User(ABC):
+    def __init__(self, name, phone, email, address):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+
+class Employee(User):
+    def __init__(self, name, phone, email, address, age, designation, salary):
+        super().__init__(name, phone, email, address)
+        self.age = age
+        self.designation = designation
+        self.salary = salary
+
+emp = Employee('Samin', '12352552', 'sifatsamin@gmail.com', 'Cumilla', 24, 'Waiter', 10000)
+print(emp.name)
+print(emp.email)
+print(emp.age)
+print(emp.salary)
+        
