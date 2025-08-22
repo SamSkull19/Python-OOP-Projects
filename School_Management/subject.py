1f3589053877796e52961ce5165e2dd076e1b712
sifat-samin <sifatsamin19@gmail.com>
2025-08-22 17:10:02 +0600

    Subject Class work done

--- School_Management/subject.py
+++ School_Management/subject.py
@@ -1,3 +1,5 @@
+from school import School
+
 class Subject:
     def __init__(self, name, teacher):
         self.name = name
@@ -5,7 +7,9 @@ class Subject:
         self.max_marks = 100
         self.pass_marks = 33
 
-    
+    def exam(self, students):
+        for student in students:
+            mark = self.teacher.evaluate_exam()
+            student.marks[self.name] = mark
+            student.subject_grade[self.name] = School.calculate_grade(mark)
 
-
-        
