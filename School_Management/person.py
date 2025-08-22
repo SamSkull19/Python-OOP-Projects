6bbc35c39dedae0e8949940cf2eaebfeed9465c9
sifat-samin <sifatsamin19@gmail.com>
2025-08-22 15:13:45 +0600

    Teacher Class Added

--- School_Management/person.py
+++ School_Management/person.py
@@ -1,5 +1,16 @@
+from random import randint
+
 class Person:
     def __init__(self, name):
         self.name = name
 
+class Teacher(Person):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def evaluate_exam(self):
+        return randint(1, 100)
     
+    def teach():
+        pass
+
