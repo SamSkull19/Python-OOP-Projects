7125278b0bed3957f55cc2d3d635b401e0a23f77
sifat-samin <sifatsamin19@gmail.com>
2025-08-25 23:14:33 +0600

    Set Driver and Start Driver Added

--- Ride_Sharing/ride.py
+++ Ride_Sharing/ride.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Ride:
     def __init__(self, start_location, end_location):
         self.start_location = start_location
@@ -8,3 +10,9 @@ class Ride:
         self.end_time = None
         self.estimated_time = None
 
+    def set_driver(self, driver):
+        self.driver = driver
+
+    def start_ride(self):
+        self.start_time = datetime.now()
+        
