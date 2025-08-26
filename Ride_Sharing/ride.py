8624419130e0b574eda6f52791ff5d5d4d95482d
sifat-samin <sifatsamin19@gmail.com>
2025-08-26 06:32:43 +0600

    Ride Matching Added

--- Ride_Sharing/ride.py
+++ Ride_Sharing/ride.py
@@ -31,4 +31,15 @@ class RideRequest:
         self.end_location = end_location
 
 
+class RideMatching:
+    def __init__(self, drivers):
+        self.available_drivers = drivers
+
+    def find_driver(self, ride_request):
+        if len(self.available_drivers) > 0:
+            print('Looking for drivers....')
+            driver = self.available_drivers[0]
+            ride = Ride(ride_request.rider.current_location, ride_request.end_location)
+            driver.accept_ride(ride)
+            return ride
         
