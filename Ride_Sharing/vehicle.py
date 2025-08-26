b238929f47ff8cf899b6bb99865db75000356439
sifat-samin <sifatsamin19@gmail.com>
2025-08-26 06:38:02 +0600

    New Vehicle Classes added

--- Ride_Sharing/vehicle.py
+++ Ride_Sharing/vehicle.py
@@ -12,3 +12,14 @@ class Vehicle(ABC):
         self.license_plate = license_plate
         self.rate = rate
 
+class Car(Vehicle):
+    def __init__(self, vehicle_type, license_plate, rate):
+        super().__init__(vehicle_type, license_plate, rate)
+        
+class Bike(Vehicle):
+    def __init__(self, vehicle_type, license_plate, rate):
+        super().__init__(vehicle_type, license_plate, rate)
+
+class Cng(Vehicle):
+    def __init__(self, vehicle_type, license_plate, rate):
+        super().__init__(vehicle_type, license_plate, rate)
